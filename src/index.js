1f8dc9b1d0bbb1528e4040b4691de5dac9c8a390
Q.McKinley <q.mckinley@outlook.com>
2019-06-05 19:02:10 -0400

    websockets socket.io

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+let count = 0;
+
+// server (emit) -> client (receive) - countUpdated
+// client (emit) -> server (receive) - increment
+
+io.on('connection', (socket) => {
+  console.log('New WebSocket connection');
+  socket.emit('countUpdated', count);
+  socket.on('increment', () => {
+    count++;
+    // socket.emit('countUpdated', count);
+    io.emit('countUpdated', count);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}`)
+});
